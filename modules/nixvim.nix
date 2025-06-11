7dc50a00e811e5a4eaa561f59c23bf06233b2ee6
Alfin <alfinbaiju@proton.me>
2025-06-11 15:51:19 +0400

    testing clangd changes [C.]  #1  binutils-wrapper  2.44 x2 -> 2.44 [C.]  #2  compiler-rt-libc  18.0.0-4182046534deb851753f0d962146e5176f648893, 18.0.0-4182046534deb851753f0d962146e5176f648893-dev, 19.1.7, 19.1.7-dev -> 18.0.0-4182046534deb851753f0d962146e5176f648893, 18.0.0-4182046534deb851753f0d962146e5176f648893-dev [R.]  #1  clang          19.1.7, 19.1.7-lib [R.]  #2  clang-tools    19.1.7 [R.]  #3  clang-wrapper  19.1.7

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -84,7 +84,24 @@
             installCargo = false;
             installRustc = false;
           };
-          clangd.enable = true;
+          clangd = {
+            enable = true;
+            package = null;
+            settings = {
+              cmd = [
+                "clangd"
+                "--background-index"
+              ];
+              filetypes = [
+                "c"
+                "cpp"
+              ];
+              root_markers = [
+                "compile_commands.json"
+                "compile_flags.txt"
+              ];
+            };
+          };
         };
       };
     };

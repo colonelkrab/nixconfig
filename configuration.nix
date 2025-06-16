682da7809a22f1aac9aea8e5de68a8854c8ec24f
Alfin <alfinbaiju@proton.me>
2025-06-16 17:09:27 +0400

    upgrade to kernel 6.14 [U.]  #1  initrd-linux  6.13.12 -> 6.14.11 [U.]  #2  linux         6.13.12, 6.13.12-modules, 6.13.12-modules-shrunk -> 6.14.11, 6.14.11-modules, 6.14.11-modules-shrunk

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_6_13;
+  boot.kernelPackages = pkgs.linuxPackages_6_14;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.

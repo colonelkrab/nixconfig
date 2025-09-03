bd2814cdd2faf14290a259733e32a08c6236d67a
Alfin <alfinbaiju@proton.me>
2025-09-03 11:25:18 +0530

    change nvim theme [R.]  #1  vimplugin-catppuccin-nvim  2025-05-04

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -2,7 +2,7 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
-    colorschemes.catppuccin.enable = true;
+    # colorschemes.catppuccin.enable = true;
 
     # keybindings
     keymaps = [
